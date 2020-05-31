65a6ef074f0e8ca9d31aa98a46aaa3339216e4bf
Harley Hanna <harleyhannawrit@gmail.com>
2020-05-31 15:13:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz (int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 != 0
     return "Fizz"
     
-  elsif int % 5 == 0
+  elsif int % 5 == 0 && int % 3 != 0
     return "Buzz"
     
   elsif int % 5 == 0 && int % 3 == 0
